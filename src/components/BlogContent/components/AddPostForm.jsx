ffbdabf6f4a0c78d97a635bb8980a8c6cfc88a6a
Aharon <aharong@mail.ru>
2022-03-09 22:37:18 +0400

    Made form controlable

--- src/components/BlogContent/components/AddPostForm.jsx
+++ src/components/BlogContent/components/AddPostForm.jsx
@@ -1,40 +1,64 @@
 import "./AddPostForm.css";
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import { Component } from "react";
 
-export const AddPostForm = ({ handleAddFormHide }) => {
-  return (
-    <>
-      <form action="" className="addPostForm">
-        <buton className="hideBtn" onClick={handleAddFormHide}>
-          <CloseRoundedIcon />
-        </buton>
-        <h2>New Post</h2>
-        <div>
-          <input
-            className="addFormInput"
-            type="text"
-            name="postTitle"
-            placeholder="Enter Post Name"
-          />
-        </div>
-        <div>
-          <textarea
-            className="addFormInput"
-            name="postDescription"
-            placeholder="Enter New Post "
-          />
-        </div>
-        <div>
-          <button
-            onClick={handleAddFormHide}
-            className="blackbtn"
-            type="button"
-          >
-            Add Post
-          </button>
-        </div>
-      </form>
-      <div  className="overlay"></div>
-    </>
-  );
-};
+export class AddPostForm extends Component {
+  state = {
+    postTitle: "",
+    postDescription: "",
+  };
+  handelPostTitleChange = (e) => {
+    this.setState({
+      postTitle: e.target.value,
+    });
+  };
+
+  handelPostDescription = (e) => {
+    this.setState({
+      postDescription: e.target.value,
+    });
+  };
+
+  render() {
+    const handleAddFormHide = this.props.handleAddFormHide;
+    return (
+      <>
+        <form action="" className="addPostForm">
+          <buton className="hideBtn" onClick={handleAddFormHide}>
+            <CloseRoundedIcon />
+          </buton>
+          <h2>New Post</h2>
+          <div>
+            <input
+              className="addFormInput"
+              type="text"
+              name="postTitle"
+              placeholder="Enter Post Name"
+              value={this.state.postTitle}
+              onChange={this.handelPostTitleChange}
+            />
+          </div>
+          <div>
+            <textarea
+              className="addFormInput"
+              name="postDescription"
+              placeholder="Enter New Post "
+              value={this.state.postDescription}
+              onChange={this.handelPostDescription}
+            />
+          </div>
+          <div>
+            <button
+              onClick={handleAddFormHide}
+              className="blackbtn"
+              type="button"
+            >
+              Add Post
+            </button>
+          </div>
+        </form>
+        <div className="overlay"></div>
+      </>
+    );
+  }
+}
